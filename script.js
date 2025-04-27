75a946dc21e354805943905260b22181bd65347a
Bruno Freijomil <bruno_freijomil@hotmail.com>
2025-04-27 16:53:40 -0300

    commit

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const containerTabla = document.querySelector('.container-tabla');
+const btnAgregar = document.getElementById("btnAgregar");
+const containerForm = document.querySelector('.container-form');
+const textoForm = document.getElementById("texto-form")
+const btnAceptar = document.getElementById("btnAceptar");
+const btnCancelar = document.getElementById("btnCancelar");
+const spinner = document.getElementById("spinner");
+
+//    ************* CREAR CLASES *************
 class Persona {
     constructor(id, nombre, apellido, edad) {
         this.id = id;
@@ -37,3 +46,43 @@ class Profesional extends Persona {
         return `ID: ${this.id}, Nombre: ${this.nombre}, Apellido: ${this.apellido}, Edad: ${this.edad}, Título: ${this.titulo}, Facultad: ${this.facultad}, Graduación: ${this.graduacion}`;
     }
 }
+
+
+
+
+// MOSTRAR TABLA O MOSTRAR FORMULARIO
+function showVistaTabla(mostrar) {
+    if (mostrar) {
+        containerTabla.classList.remove('hidden');
+        containerForm.classList.add('hidden');
+    } else {
+        containerTabla.classList.add('hidden');
+        containerForm.classList.remove('hidden');
+    }
+}
+
+
+
+
+
+//    ************* AGREGAR ELEMENTO *************
+btnAgregar.addEventListener("click", function () {
+    showVistaTabla(false);
+    textoForm.innerHTML = "(ALTA ELEMENTO)";
+    console.log("Agregando elemento...");
+});
+
+function agregarElemento() {
+    //mostrar únicamente los campos correspondientes al tipo de elemento que se está insertando.
+}
+
+btnAceptar.addEventListener("click", function (e) {
+    /*
+    Al  hacer  Click  en  "Aceptar",  bloquear  la  pantalla  con  el  contenedor  "Spinner",  y  realizar  la  solicitud  al  API  sobre  el  endpoint 
+PersonasFutbolistasProfesionales.php,  con  el  Verbo  PUT,  con  encabezado  Content-Type  de  valor  application/json  y  cuerpo  un 
+string de objeto JSON que representa los atributos del elemento a insertar SIN el ID.
+    */
+    e.preventDefault();
+    spinner.classList.remove('hidden');
+
+});
